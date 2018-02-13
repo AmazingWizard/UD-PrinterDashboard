a2a4ebff08f7daf0db17e6c1b5062a6e46baa2f6
Tom E. Pomeroy <pomeroyte@mansfieldct.org>
2018-02-13 10:33:37 -0500

    Update to current UD version. Check Change Log.

--- Printer-Dashboard.ps1
+++ Printer-Dashboard.ps1
@@ -14,8 +14,6 @@ $Printers = Get-Content "$DataPath" | ConvertFrom-Json
 
 Start-UDDashboard -port $i -Content {
     New-UDDashboard -Title $Config.DashboardName -NavBarColor '#011721' -NavBarFontColor "#CCEDFD" -BackgroundColor "White" -FontColor "#011721" -Content {
-
-        # Under 10% Toner Cards
         New-UDRow {
             New-UDColumn -Size 3 {
                 New-UDCounter -AutoRefresh -RefreshInterval 5 -Title "Black Below 10%" -BackgroundColor "#1d1e21" -FontColor "#eaeaea" -Endpoint {
@@ -24,59 +22,57 @@ Start-UDDashboard -port $i -Content {
             }
             New-UDColumn -Size 3 {
                 New-UDCounter -AutoRefresh -RefreshInterval 5 -Title "Cyan Below 10%" -BackgroundColor "#42d4f4" -FontColor "#080e1c" -Endpoint {
-                    (((get-content -Path "$DataPath" | ConvertFrom-json).Toner | Where-Object {$_.Cyan -lt 10}) | Measure-Object).Count
+                    (($(get-content -Path "$DataPath" | ConvertFrom-json).Toner | Where-Object {$_.Cyan -lt 10}) | Measure-Object).Count
                 }
             }
             New-UDColumn -Size 3 {
                 New-UDCounter -AutoRefresh -RefreshInterval 5 -Title "Magenta Below 10%" -BackgroundColor "#ce3ef2" -FontColor "#080e1c" -Endpoint {
-                    (((get-content -Path "$DataPath" | ConvertFrom-json).Toner | Where-Object {$_.Magenta -lt 10}) | Measure-Object).Count
+                    (($(get-content -Path "$DataPath" | ConvertFrom-json).Toner | Where-Object {$_.Magenta -lt 10}) | Measure-Object).Count
                 }
             }
             New-UDColumn -Size 3 {
                 New-UDCounter -AutoRefresh -RefreshInterval 5 -Title "Yellow Below 10%" -BackgroundColor "#f1d03e" -FontColor "#080e1c" -Endpoint {
-                    (((get-content -Path "$DataPath" | ConvertFrom-json).Toner | Where-Object {$_.Yellow -lt 10}) | Measure-Object).Count
+                    (($(get-content -Path "$DataPath" | ConvertFrom-json).Toner | Where-Object {$_.Yellow -lt 10}) | Measure-Object).Count
                 }
             }
         }
-
-        # Leaderboards for Lowest Toner by Type
         New-UDRow {
             New-UDColumn -Size 3 {
-                New-UDGrid -Title "Lowest Black Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Black") -AutoRefresh -RefreshInterval 5 -BackgroundColor "#1d1e21" -FontColor "#eaeaea" -Endpoint {
-                    (get-content -path $datapath | ConvertFrom-Json) | Sort-Object {$_.Toner.Black} | Select-Object -First 5 | Out-UDGridData
+                New-UDGrid -Title "Lowest Black Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Black") -AutoRefresh -RefreshInterval 5 -BackgroundColor "#1d1e21" -FontColor "#eaeaea" -DefaultSortColumn 1 -Endpoint {
+                    (get-content -path $datapath |ConvertFrom-Json) | Sort-Object {$_.Toner.Black} | Select-Object -First 5 | Out-UDGridData
                 }
             }
             New-UDColumn -Size 3 {
-                New-UDGrid -Title "Lowest Cyan Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Cyan") -AutoRefresh -RefreshInterval 5 -BackgroundColor "#42d4f4" -FontColor "#080e1c" -Endpoint {
-                    (get-content -path $datapath | ConvertFrom-Json) | Sort-Object {$_.Toner.Cyan} | Select-Object -First 5 | Out-UDGridData
+                New-UDGrid -Title "Lowest Cyan Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Cyan") -AutoRefresh -RefreshInterval 5 -BackgroundColor "#42d4f4" -FontColor "#080e1c" -DefaultSortColumn 1 -Endpoint {
+                    (get-content -path $datapath |ConvertFrom-Json) | Sort-Object {$_.Toner.Cyan} | Select-Object -First 5 | Out-UDGridData
                 }
             }
             New-UDColumn -Size 3 {
-                New-UDGrid -Title "Lowest Magenta Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Magenta") -AutoRefresh -RefreshInterval 5 -BackgroundColor "#ce3ef2" -FontColor "#080e1c" -Endpoint {
+                New-UDGrid -Title "Lowest Magenta Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Magenta") -AutoRefresh -RefreshInterval 5 -BackgroundColor "#ce3ef2" -FontColor "#080e1c" -DefaultSortColumn 1 -Endpoint {
                     (get-content -path $datapath |ConvertFrom-Json) | Sort-Object {$_.Toner.Magenta} | Select-Object -First 5 | Out-UDGridData
                 }
             }
             New-UDColumn -Size 3 {
-                New-UDGrid -Title "Lowest Yellow Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Yellow") -AutoRefresh -RefreshInterval 5  -BackgroundColor "#f1d03e" -FontColor "#080e1c" -Endpoint {
+                New-UDGrid -Title "Lowest Yellow Toner" -Headers @("Name", "Percent Left") -Properties @("Name", "Toner.Yellow") -AutoRefresh -RefreshInterval 5 -BackgroundColor "#f1d03e" -FontColor "#080e1c" -DefaultSortColumn 1 -Endpoint {
                     (get-content -path $datapath |ConvertFrom-Json) | Sort-Object {$_.Toner.Yellow} | Select-Object -First 5 | Out-UDGridData
                 }
             }
         }
 
-        # Printer Charts for Toner Levels
         New-UDRow {
+            $MinMaxAxis = New-UDLinearChartAxis -Minimum 0 -Maximum 100
+            $Options = New-UDBarChartOptions -yAxes $MinMaxAxis
             foreach ($Printer in $Printers) {
+                $Links = @(New-UDLink -Text $($Printer.Address)  -Url "Http://$($Printer.Address)/" -Icon globe -OpenInNewWindow; New-UDLink -Text $($Printer.Type) -Url "#")
                 New-UDColumn -Size 3 {
-                    New-UDChart -Title $Printer.Name @colors -type Bar -AutoRefresh -RefreshInterval 5 -Endpoint {
+                    New-UDChart -Title $Printer.Name @colors -type Bar -AutoRefresh -RefreshInterval 5  -Links $links -Endpoint {
                         $($(Get-Content "$DataPath" | ConvertFrom-Json) | Where-Object {$_.Name -like $Printer.Name}).Toner | Out-UDChartData -LabelProperty "Name" -Dataset @(
-                            New-UDChartDataset -DataProperty "Black" -Label "Black" -BackgroundColor "#080e1c" -HoverBackgroundColor "#080e1c" -
-                            New-UDChartDataset -DataProperty "Cyan" -Label "Cyan" -BackgroundColor "#42d4f4" -HoverBackgroundColor "#42d4f4"
-                            New-UDChartDataset -DataProperty "Magenta" -Label "Magenta" -BackgroundColor "#ce3ef2" -HoverBackgroundColor "#ce3ef2"
-                            New-UDChartDataset -DataProperty "Yellow" -Label "Yellow" -BackgroundColor "#f1d03e" -HoverBackgroundColor "#f1d03e"
-                            New-UDChartDataset -DataProperty "Max" -Label "Max" -BackgroundColor "wite" -HoverBackgroundColor "wite" -
-                            New-UDChartDataset -DataProperty "Min" -Label "Min" -BackgroundColor "wite" -HoverBackgroundColor "wite" -
+                            New-UDBarChartDataset -DataProperty "Black" -Label "Black" -BackgroundColor "#080e1c" -HoverBackgroundColor "#080e1c" -
+                            New-UDBarChartDataset -DataProperty "Cyan" -Label "Cyan" -BackgroundColor "#42d4f4" -HoverBackgroundColor "#42d4f4"
+                            New-UDBarChartDataset -DataProperty "Magenta" -Label "Magenta" -BackgroundColor "#ce3ef2" -HoverBackgroundColor "#ce3ef2"
+                            New-UDBarChartDataset -DataProperty "Yellow" -Label "Yellow" -BackgroundColor "#f1d03e" -HoverBackgroundColor "#f1d03e"
                         )
-                    }
+                    } -Options $Options
                 }
             }
         }
--- changelog.md
+++ changelog.md
@@ -1,3 +1,9 @@
+# Change Log: 2018-02-13
+* Updated script to be compatable with current version of UD.
+* Added quick links to the Toner Graphs that take you to the printers web interface.
+* Added Printer Device Name in the Links section (Currently is a nul link)
+* Set the DefaultSortColumn for the now sortable UD Grids.
+
 # Change Log: 2017-11-21
 Fixed an error in the toner leaderboards that caused the data to not refresh.
 
